b3c1e32439288e4f7760458d9f438fe49358567e
Manuel Fernandes <a23502@alunos.ipca.pt>
2024-12-10 23:32:18 +0000

    Updated README.md

--- RESTful-API/obj/Debug/net8.0/RESTful-API.AssemblyInfo.cs
+++ RESTful-API/obj/Debug/net8.0/RESTful-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RESTful-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be8511cd151ddd16862a2e575b5893090d27af5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb6004c62865d246441f2ddc6fc2dae0a6701567")]
 [assembly: System.Reflection.AssemblyProductAttribute("RESTful-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RESTful-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
